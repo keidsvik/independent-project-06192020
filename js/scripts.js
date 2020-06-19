6bec15b1fecf5e2dc357f6f732647d7ef7688619
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-19 14:59:29 -0700

    add event.preventDefault

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
 //BUSINESS LOGIC
-function Pizza() {
+
+//object literal to store pizza sizes and prices for toppings
+//let pizzaPrices = {
+  //small: 9,
+  //medium: 12,
+  //large: 15,
+  //toppings: 1,
+//}
+
+//pizza constructor with size and array of toppings stored here
+function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = [];
+  this.toppings = toppings;
 }
 
-Pizza.prototype.addTopping(topping) {
-  this.toppings.push(topping);
-}
+//Pizza.prototype.addATopping = function(topping) {
+ // this.toppings.push(topping);
+//}
 
-Pizza.prototype.pizzaPrice
+Pizza.prototype.price = function() {
+  let priceBase = 9
 
-function Toppings() {
-  this.toppingName;
-}
-
-let pizzaPrices = {
-  small: 9,
-  medium: 12,
-  large: 15,
-  toppings: 1,
+  if (this.size() === small) {
+    priceBase = 9;
+  } else if (this.size() === medium) {
+    priceBase = 12;
+  } else {
+    priceBase = 15;
+  } 
+   return priceBase;
 }
 
 //USER INTERFACE AKA "THE PARTY"
@@ -27,9 +37,10 @@ $(document).ready(function() {
   $("#pizzaform").submit(function(event) {
     let toppings = $("#toppings").val();
     let size = $("#size").val();
-    let yourPizza = new Pizza(size, toppings);
-    let pizzaPrice = yourPizza.price();
-    console.log(pizzaPrice);
+    let completePizzaOrder = new Pizza(size, toppings);
+    let pizzaOrderPrice = completePizzaOrder.price();
+    console.log(pizzaOrderPrice);
+    event.preventDefault();
   });
 });
 
