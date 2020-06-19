0d8eb6047b90bbe7a5ceece47140b2a234af83ea
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-19 11:45:15 -0700

    add in pizza constructor along with a topping prototype and a price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,30 @@
 //BUSINESS LOGIC
+function Pizza() {
+  this.size = size;
+  this.toppings = [];
+}
 
+Pizza.prototype.addTopping(topping) {
+  this.toppings.push(topping);
+}
+
+Pizza.prototype.pizzaPrice
+
+function Toppings() {
+  this.toppingName;
+}
+
+let pizzaPrices = {
+  small: 9,
+  medium: 12,
+  large: 15,
+  toppings: 1,
+}
 
 //USER INTERFACE AKA "THE PARTY"
 
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("#pizzaform").submit(function(event) {
     let toppings = $("#toppings").val();
     let size = $("#size").val();
     let yourPizza = new Pizza(size, toppings);
