447cbd18ed5440546abdf79f7107e60acc8296ac
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-19 11:20:50 -0700

    scrap previous code in js document to start over

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,17 @@
 //BUSINESS LOGIC
-function Pizza (toppings, size) {
-  this.toppings = toppings;
-  this.size = size;
-}
-
-Pizza.prototype.pizzaPrice = function(){
-  let price = 8;
-
-  if (this.size === "Small") {
-    price = price;
-  } else if (this.size === "Medium") {
-    price = price + 3;
-  } else {
-    price = price + 4;
-  }
-}
-
 
 
 //USER INTERFACE AKA "THE PARTY"
 
 $(document).ready(function() {
   $("form").submit(function(event) {
+    let toppings = $("#toppings").val();
     let size = $("#size").val();
-    let topping1 = $("#topping1").val();
-    let topping2 = $("#topping2").val();
-    let topping3 = $("#topping3").val();
-    let yourPizza = new Pizza(toppings, size);
-    let yourPizzaPrice = yourPizza.price();
+    let yourPizza = new Pizza(size, toppings);
+    let pizzaPrice = yourPizza.price();
+    console.log(pizzaPrice);
   });
 });
+
+
+
