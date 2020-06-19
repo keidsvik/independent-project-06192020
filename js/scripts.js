05d3703111dc427f65fb8b974e20a5ea9446adb8
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-19 09:24:28 -0700

    add variables to user inerface section of js logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,29 @@ function Pizza (toppings, size) {
   this.size = size;
 }
 
-Pizza.prototype.pizzaPrice = function(){}
+Pizza.prototype.pizzaPrice = function(){
+  let price = 8;
+
+  if (this.size === "Small") {
+    price = price;
+  } else if (this.size === "Medium") {
+    price = price + 3;
+  } else {
+    price = price + 4;
+  }
+}
 
 
 
 //USER INTERFACE AKA "THE PARTY"
 
 $(document).ready(function() {
-  $("form#????").submit(function(event) {
-
+  $("form").submit(function(event) {
+    let size = $("#size").val();
+    let topping1 = $("#topping1").val();
+    let topping2 = $("#topping2").val();
+    let topping3 = $("#topping3").val();
+    let yourPizza = new Pizza(toppings, size);
+    let yourPizzaPrice = yourPizza.price();
   });
 });
